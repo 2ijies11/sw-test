10f6c2b52aa47ab25071140a040f16435241c34b
2ijies11 <kgseijin112@gmail.com>
2021-11-23 06:08:39 +0900

    register files

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const BASE_URL = '2ijies11.github.io/sw-test/'
+const preCache = [
+    '',
+    'index.html',
+    'script/main.js',
+];
+
+self.addEventListener('install', function (event) {
+    console.log('sw.js install');
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(preCache.map(file => `${BASE_URL}${file}`));
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
